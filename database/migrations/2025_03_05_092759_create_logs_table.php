eeb6d447b8cd3b7c0155793f4235fc1c154ca589
Ihor Boiko <ihor.boiko@appflame.com>
2025-03-10 11:30:27 +0200

    log to database

---
+++ database/migrations/2025_03_05_092759_create_logs_table.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    private const TABLE = 'logs';
+
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        if (false === Schema::hasTable(self::TABLE)) {
+            Schema::create(self::TABLE, function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('channel')->nullable();
+                $table->json('context')->nullable();
+                $table->decimal('created_at', 15, 4);
+                $table->string('datetime')->nullable();
+                $table->json('extra')->nullable();
+                $table->text('formatted')->nullable();
+                $table->integer('level')->default(0);
+                $table->string('level_name', 20);
+                $table->text('message')->nullable();
+                $table->decimal('updated_at', 15, 4);
+            });
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
