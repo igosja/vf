8dfa2399a7c47ff9ce9076f16e7792a2a301059d
Ihor Boiko <ihor.boiko@appflame.com>
2025-05-19 20:44:53 +0300

    roster page

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,46 +1,83 @@
+import {Row, Col, Table, Avatar} from "antd";
+
 export default function Home() {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna odio, congue vitae arcu ut, sodales
-                sollicitudin magna. Integer eu sem risus. Mauris non pulvinar massa, vitae consectetur mi. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi ut fermentum ipsum.
-                Pellentesque a ligula lacus. Etiam in libero at lacus vulputate eleifend eget non enim. Proin commodo
-                commodo nulla nec maximus. Cras id lectus rutrum, pharetra ex sit amet, egestas nisl. Suspendisse
-                volutpat, augue sollicitudin blandit finibus, libero tellus tincidunt nisi, at egestas tellus tortor
-                auctor ipsum.</p>
-
-            <p>Proin ligula libero, finibus et convallis vel, finibus sit amet sem. Nullam porttitor vel lorem ut
-                dignissim. Integer id semper ligula, non commodo velit. Integer nec arcu ex. Fusce auctor sit amet
-                mauris eget vulputate. Aenean hendrerit a diam id consequat. Maecenas vel ultricies eros. Vivamus velit
-                quam, sodales id ligula in, dignissim pulvinar massa. Nulla egestas fringilla ipsum vulputate finibus.
-                Nulla cursus, turpis sit amet consectetur aliquet, sem metus auctor eros, id congue sapien nulla quis
-                nibh. Fusce finibus scelerisque dignissim. Nam vel sapien ultrices, tincidunt nulla in, laoreet nisl.
-                Vestibulum sapien ipsum, consequat non quam eget, lacinia feugiat quam. Phasellus a purus a odio
-                dignissim elementum sed non nisl. Ut volutpat ipsum sapien, ut laoreet ante scelerisque non. Suspendisse
-                potenti.</p>
-
-            <p>Pellentesque rutrum nulla vel turpis volutpat dapibus. In vitae eleifend arcu, vitae eleifend augue.
-                Donec mi libero, rutrum nec nisl ut, suscipit ornare dui. Donec vel commodo ipsum. Nulla massa libero,
-                faucibus vitae faucibus nec, condimentum ac neque. Donec lectus quam, ullamcorper ut tempus at, cursus
-                vitae enim. Duis viverra rutrum sapien non maximus. Pellentesque mi mi, pharetra at vestibulum eget,
-                sagittis ac ex.</p>
-
-            <p>Maecenas id rutrum diam. Integer bibendum mauris ac nisi faucibus, sit amet maximus lectus gravida.
-                Maecenas placerat dictum nisi, et vestibulum libero rhoncus sit amet. Ut lacinia lorem at consequat
-                convallis. Vestibulum aliquam dolor eu posuere eleifend. Ut laoreet vel leo eget interdum. Integer
-                viverra elementum quam a euismod. Duis lacus arcu, posuere sed cursus vestibulum, rhoncus sed erat.
-                Nullam quis ligula ac est consequat efficitur eget sed diam. Donec lacinia nulla id efficitur ultricies.
-                Donec luctus lacus turpis, sit amet cursus nisi pellentesque a. Sed interdum vestibulum enim, quis
-                blandit velit varius id. Nunc vehicula nibh in sapien dignissim, consectetur tristique diam
-                bibendum.</p>
-
-            <p>Vivamus lectus nunc, dapibus sit amet vehicula non, lobortis non ipsum. Sed at dapibus mi. Nam in feugiat
-                libero, a fermentum sapien. Fusce feugiat ut quam quis ornare. Curabitur a massa eu arcu ullamcorper
-                rutrum. Suspendisse eu aliquam ligula, eget molestie felis. Etiam in arcu et turpis pharetra venenatis
-                eu interdum sem. Mauris tincidunt nec augue eu congue. Fusce sit amet ipsum interdum, pulvinar magna
-                vitae, iaculis orci. Proin eu vulputate massa. Ut id auctor nisi. Sed vestibulum nec justo quis maximus.
-                Duis condimentum odio id metus tincidunt, quis feugiat lacus tempus. Phasellus id lectus enim. Donec at
-                condimentum orci, ut tincidunt quam. Aliquam ut nisi ut erat mollis dictum.</p>
-        </div>
+        <Row>
+            <Col span={24}>
+                <Row>
+                    <Col span={12} style={{ textAlign: "center"}}>
+                        <Avatar
+                            size={200}
+                            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                        />
+                    </Col>
+                    <Col span={12}>
+                        <Table
+                            showHeader={false}
+                            pagination={false}
+                            columns={[
+                                {
+                                    dataIndex: 'field',
+                                    key: 'field',
+                                },
+                                {
+                                    dataIndex: 'value',
+                                    key: 'value',
+                                },
+                            ]}
+                            dataSource={[
+                                {
+                                    key: '1',
+                                    field: 'Name',
+                                    value: 'Real Madrid',
+                                },
+                                {
+                                    key: '2',
+                                    field: 'Stadium',
+                                    value: 50123,
+                                },
+                            ]}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Table
+                            columns={[
+                                {
+                                    title: 'Name',
+                                    dataIndex: 'name',
+                                    key: 'name',
+                                },
+                                {
+                                    title: 'Age',
+                                    dataIndex: 'age',
+                                    key: 'age',
+                                },
+                                {
+                                    title: 'Address',
+                                    dataIndex: 'address',
+                                    key: 'address',
+                                },
+                            ]}
+                            dataSource={[
+                                {
+                                    key: '1',
+                                    name: 'Mike',
+                                    age: 32,
+                                    address: '10 Downing Street',
+                                },
+                                {
+                                    key: '2',
+                                    name: 'John',
+                                    age: 42,
+                                    address: '10 Downing Street',
+                                },
+                            ]}
+                        />
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     );
 }
