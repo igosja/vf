4eda6ce1fce8b1e5af6acd3f7a0596803e33b94f
Ihor Boiko <ihor.boiko@appflame.com>
2026-03-17 16:48:37 +0200

    generator
    Signed-off-by: Ihor Boiko <ihor.boiko@appflame.com>

--- backend/app/Console/Commands/Generator.php
+++ backend/app/Console/Commands/Generator.php
@@ -25,13 +25,14 @@ class Generator extends Command
      */
     public function handle()
     {
-        $historyCup = [];
-        $history = [];
-        for ($i = 1; $i <= 16; $i++) {
-            $historyCup['team' . $i] = 0;
-            $history['team' . $i] = 0;
-        }
+//        $this->generateGame();
+        $this->generateChampionship();
+//        $this->generateChampionshipHistory();
+//        $this->generateCupHistory();
+    }
 
+    private function generateGame()
+    {
         $bar = $this->output->createProgressBar(10000);
         $bar->start();
 
@@ -42,12 +43,11 @@ class Generator extends Command
         ];
 
         for ($y = 1; $y <= 10000; $y++) {
-
             $win1 = 0;
             $win2 = 0;
-            $power1 = 3000;
-            $power2 = 3100;
-            for ($i = 0; $i < 30; $i++) {
+            $power1 = 2000 * 1.05;
+            $power2 = 3000;
+            for ($i = 0; $i < 41; $i++) {
                 if (mt_rand(0, $power1 + $power2) < $power1) {
                     $win1++;
                 } else {
@@ -55,7 +55,7 @@ class Generator extends Command
                 }
             }
 
-            if ($win1 > $win2 + 1) {
+            if ($win1 > $win2 + 2) {
                 $oneGameResult['win']++;
             } elseif ($win1 + 2 < $win2) {
                 $oneGameResult['lose']++;
@@ -66,172 +66,269 @@ class Generator extends Command
             $bar->advance();
         }
 
-//        for ($y = 1; $y <= 10000; $y++) {
-//            $cup = [];
-//            $table = [];
-//            $result = [];
-//            for ($i = 1; $i <= 16; $i++) {
-//                $cup[] = [
-//                    'team' => 'team' . $i,
-//                    'power' => 3600 - $i * 100,
-//                ];
-//                $table['team' . $i] = [
-//                    'power' => 3600 - $i * 100,
-//                ];
-//                $result['team' . $i] = [
-//                    'game' => 0,
-//                    'win' => 0,
-//                    'draw' => 0,
-//                    'lose' => 0,
-//                    'points' => 0,
-//                    'power' => 3600 - $i * 100,
-//                ];
-//            }
-//            foreach ($table as $teamHome => $itemHome) {
-//                foreach ($table as $teamAway => $itemAway) {
-//                    if ($teamHome == $teamAway) {
-//                        continue;
-//                    }
-//
-//                    $win1 = 0;
-//                    $win2 = 0;
-//                    $power1 = $itemHome['power'];
-//                    $power2 = $itemAway['power'];
-//                    for ($i = 0; $i < 30; $i++) {
-//                        if (mt_rand(0, $power1 + $power2) < $power1) {
-//                            $win1++;
-//                        } else {
-//                            $win2++;
-//                        }
-//                    }
-//
-//                    $result[$teamHome]['game']++;
-//                    $result[$teamAway]['game']++;
-//
-//                    if ($win1 > $win2 + 1) {
-//                        $result[$teamHome]['win']++;
-//                        $result[$teamHome]['points'] += 3;
-//                        $result[$teamAway]['lose']++;
-//                    } elseif ($win1 + 2 < $win2) {
-//                        $result[$teamAway]['win']++;
-//                        $result[$teamAway]['points'] += 3;
-//                        $result[$teamHome]['lose']++;
-//                    } else {
-//                        $result[$teamHome]['draw']++;
-//                        $result[$teamHome]['points'] += 1;
-//                        $result[$teamAway]['draw']++;
-//                        $result[$teamAway]['points'] += 1;
-//                    }
-//                }
-//            }
-//            uasort($result, function ($a, $b) {
-//                return $b['points'] <=> $a['points'];
-//            });
-//            $history[array_key_first($result)]++;
-//
-//            shuffle($cup);
-//            $countCup = count($cup);
-//            for ($j = 0; $j < $countCup; $j = $j + 2) {
-//                $win1 = 0;
-//                $win2 = 0;
-//                $power1 = $cup[$j]['power'];
-//                $power2 = $cup[$j+1]['power'];
-//                for ($i = 0; $i < 31; $i++) {
-//                    if (mt_rand(0, $power1 + $power2) < $power1) {
-//                        $win1++;
-//                    } else {
-//                        $win2++;
-//                    }
-//                }
-//
-//                if ($win1 > $win2) {
-//                    unset($cup[$j+1]);
-//                } else {
-//                    unset($cup[$j]);
-//                }
-//            }
-//
-//            shuffle($cup);
-//            $countCup = count($cup);
-//            for ($j = 0; $j < $countCup; $j = $j + 2) {
-//                $win1 = 0;
-//                $win2 = 0;
-//                $power1 = $cup[$j]['power'];
-//                $power2 = $cup[$j+1]['power'];
-//                for ($i = 0; $i < 31; $i++) {
-//                    if (mt_rand(0, $power1 + $power2) < $power1) {
-//                        $win1++;
-//                    } else {
-//                        $win2++;
-//                    }
-//                }
-//
-//                if ($win1 > $win2) {
-//                    unset($cup[$j+1]);
-//                } else {
-//                    unset($cup[$j]);
-//                }
-//            }
-//
-//            shuffle($cup);
-//            $countCup = count($cup);
-//            for ($j = 0; $j < $countCup; $j = $j + 2) {
-//                $win1 = 0;
-//                $win2 = 0;
-//                $power1 = $cup[$j]['power'];
-//                $power2 = $cup[$j+1]['power'];
-//                for ($i = 0; $i < 31; $i++) {
-//                    if (mt_rand(0, $power1 + $power2) < $power1) {
-//                        $win1++;
-//                    } else {
-//                        $win2++;
-//                    }
-//                }
-//
-//                if ($win1 > $win2) {
-//                    unset($cup[$j+1]);
-//                } else {
-//                    unset($cup[$j]);
-//                }
-//            }
-//
-//            shuffle($cup);
-//            $countCup = count($cup);
-//            for ($j = 0; $j < $countCup; $j = $j + 2) {
-//                $win1 = 0;
-//                $win2 = 0;
-//                $power1 = $cup[$j]['power'];
-//                $power2 = $cup[$j+1]['power'];
-//                for ($i = 0; $i < 31; $i++) {
-//                    if (mt_rand(0, $power1 + $power2) < $power1) {
-//                        $win1++;
-//                    } else {
-//                        $win2++;
-//                    }
-//                }
-//
-//                if ($win1 > $win2) {
-//                    unset($cup[$j+1]);
-//                } else {
-//                    unset($cup[$j]);
-//                }
-//            }
-//            shuffle($cup);
-//            $historyCup[$cup[0]['team']]++;
-//
-//            $bar->advance();
-//        }
-
         $bar->finish();
         print '<pre>';
         print_r($oneGameResult);
         exit;
-//        print '<pre>';
-//        print_r($result);
-//        print '<pre>';
-//        print_r($history);
-//        print '<pre>';
-//        print_r($historyCup);
-//        exit;
+    }
+
+    private function generateChampionship()
+    {
+        $table = [];
+        $result = [];
+        for ($i = 1; $i <= 16; $i++) {
+            $table['team' . $i] = [
+                'power' => 3600 - $i * 100,
+            ];
+            $result['team' . $i] = [
+                'game' => 0,
+                'win' => 0,
+                'draw' => 0,
+                'lose' => 0,
+                'points' => 0,
+                'power' => 3600 - $i * 100,
+            ];
+        }
+        foreach ($table as $teamHome => $itemHome) {
+            foreach ($table as $teamAway => $itemAway) {
+                if ($teamHome == $teamAway) {
+                    continue;
+                }
+
+                $win1 = 0;
+                $win2 = 0;
+                $power1 = $itemHome['power'] * 1.05;
+                $power2 = $itemAway['power'];
+                for ($i = 0; $i < 41; $i++) {
+                    if (mt_rand(0, $power1 + $power2) < $power1) {
+                        $win1++;
+                    } else {
+                        $win2++;
+                    }
+                }
+
+                $result[$teamHome]['game']++;
+                $result[$teamAway]['game']++;
+
+                if ($win1 > $win2 + 2) {
+                    $result[$teamHome]['win']++;
+                    $result[$teamHome]['points'] += 3;
+                    $result[$teamAway]['lose']++;
+                } elseif ($win1 + 2 < $win2) {
+                    $result[$teamAway]['win']++;
+                    $result[$teamAway]['points'] += 3;
+                    $result[$teamHome]['lose']++;
+                } else {
+                    $result[$teamHome]['draw']++;
+                    $result[$teamHome]['points'] += 1;
+                    $result[$teamAway]['draw']++;
+                    $result[$teamAway]['points'] += 1;
+                }
+            }
+        }
+        uasort($result, function ($a, $b) {
+            return $b['points'] <=> $a['points'];
+        });
+
+        print '<pre>';
+        print_r($result);
+        exit;
+    }
+
+    private function generateChampionshipHistory()
+    {
+        $historyCup = [];
+        $history = [];
+        for ($i = 1; $i <= 16; $i++) {
+            $historyCup['team' . $i] = 0;
+            $history['team' . $i] = 0;
+        }
+
+        $bar = $this->output->createProgressBar(10000);
+        $bar->start();
+
+        for ($y = 1; $y <= 10000; $y++) {
+            $table = [];
+            $result = [];
+            for ($i = 1; $i <= 16; $i++) {
+                $table['team' . $i] = [
+                    'power' => 3600 - $i * 100,
+                ];
+                $result['team' . $i] = [
+                    'game' => 0,
+                    'win' => 0,
+                    'draw' => 0,
+                    'lose' => 0,
+                    'points' => 0,
+                    'power' => 3600 - $i * 100,
+                ];
+            }
+            foreach ($table as $teamHome => $itemHome) {
+                foreach ($table as $teamAway => $itemAway) {
+                    if ($teamHome == $teamAway) {
+                        continue;
+                    }
+
+                    $win1 = 0;
+                    $win2 = 0;
+                    $power1 = $itemHome['power'] * 1.05;
+                    $power2 = $itemAway['power'];
+                    for ($i = 0; $i < 41; $i++) {
+                        if (mt_rand(0, $power1 + $power2) < $power1) {
+                            $win1++;
+                        } else {
+                            $win2++;
+                        }
+                    }
+
+                    $result[$teamHome]['game']++;
+                    $result[$teamAway]['game']++;
+
+                    if ($win1 > $win2 + 2) {
+                        $result[$teamHome]['win']++;
+                        $result[$teamHome]['points'] += 3;
+                        $result[$teamAway]['lose']++;
+                    } elseif ($win1 + 2 < $win2) {
+                        $result[$teamAway]['win']++;
+                        $result[$teamAway]['points'] += 3;
+                        $result[$teamHome]['lose']++;
+                    } else {
+                        $result[$teamHome]['draw']++;
+                        $result[$teamHome]['points'] += 1;
+                        $result[$teamAway]['draw']++;
+                        $result[$teamAway]['points'] += 1;
+                    }
+                }
+            }
+            uasort($result, function ($a, $b) {
+                return $b['points'] <=> $a['points'];
+            });
+            $history[array_key_first($result)]++;
+
+            $bar->advance();
+        }
+
+        $bar->finish();
+        print '<pre>';
+        print_r($history);
+        exit;
+    }
+
+    private function generateCupHistory()
+    {
+        $history = [];
+        for ($i = 1; $i <= 16; $i++) {
+            $history['team' . $i] = 0;
+        }
+
+        $bar = $this->output->createProgressBar(10000);
+        $bar->start();
+
+        for ($y = 1; $y <= 10000; $y++) {
+            $cup = [];
+            for ($i = 1; $i <= 16; $i++) {
+                $cup[] = [
+                    'team' => 'team' . $i,
+                    'power' => 3600 - $i * 100,
+                ];
+            }
+
+            shuffle($cup);
+            $countCup = count($cup);
+            for ($j = 0; $j < $countCup; $j = $j + 2) {
+                $win1 = 0;
+                $win2 = 0;
+                $power1 = $cup[$j]['power'] * 1.05;
+                $power2 = $cup[$j+1]['power'];
+                for ($i = 0; $i < 31; $i++) {
+                    if (mt_rand(0, $power1 + $power2) < $power1) {
+                        $win1++;
+                    } else {
+                        $win2++;
+                    }
+                }
+
+                if ($win1 > $win2) {
+                    unset($cup[$j+1]);
+                } else {
+                    unset($cup[$j]);
+                }
+            }
+
+            shuffle($cup);
+            $countCup = count($cup);
+            for ($j = 0; $j < $countCup; $j = $j + 2) {
+                $win1 = 0;
+                $win2 = 0;
+                $power1 = $cup[$j]['power'] * 1.05;
+                $power2 = $cup[$j+1]['power'];
+                for ($i = 0; $i < 41; $i++) {
+                    if (mt_rand(0, $power1 + $power2) < $power1) {
+                        $win1++;
+                    } else {
+                        $win2++;
+                    }
+                }
+
+                if ($win1 > $win2) {
+                    unset($cup[$j+1]);
+                } else {
+                    unset($cup[$j]);
+                }
+            }
+
+            shuffle($cup);
+            $countCup = count($cup);
+            for ($j = 0; $j < $countCup; $j = $j + 2) {
+                $win1 = 0;
+                $win2 = 0;
+                $power1 = $cup[$j]['power'] * 1.05;
+                $power2 = $cup[$j+1]['power'];
+                for ($i = 0; $i < 41; $i++) {
+                    if (mt_rand(0, $power1 + $power2) < $power1) {
+                        $win1++;
+                    } else {
+                        $win2++;
+                    }
+                }
+
+                if ($win1 > $win2) {
+                    unset($cup[$j+1]);
+                } else {
+                    unset($cup[$j]);
+                }
+            }
+
+            shuffle($cup);
+            $countCup = count($cup);
+            for ($j = 0; $j < $countCup; $j = $j + 2) {
+                $win1 = 0;
+                $win2 = 0;
+                $power1 = $cup[$j]['power'] * 1.05;
+                $power2 = $cup[$j+1]['power'];
+                for ($i = 0; $i < 41; $i++) {
+                    if (mt_rand(0, $power1 + $power2) < $power1) {
+                        $win1++;
+                    } else {
+                        $win2++;
+                    }
+                }
+
+                if ($win1 > $win2) {
+                    unset($cup[$j+1]);
+                } else {
+                    unset($cup[$j]);
+                }
+            }
+            shuffle($cup);
+            $history[$cup[0]['team']]++;
+
+            $bar->advance();
+        }
+
+        $bar->finish();
+        print '<pre>';
+        print_r($history);
+        exit;
     }
 }
